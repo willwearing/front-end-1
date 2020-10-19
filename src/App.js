a81f4d2b4fe9249e96d901eae172b2279c66f1ae
Daniel Vargas <davarg5@yahoo.com>
2020-10-18 21:12:33 -0400

    added import statements and created LoginForm.js and SignUpForm.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState, useEffect } from 'react';
+import { Route, Link } from 'react-router-dom';
+import Form from './Form';
+import * as yup from 'yup';
+import axios from 'axios';
+import styled from 'styled-components'
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/*Header/Nav*/}
+
+      {/*Login Form*/}
+      <Route path='/login'>
+        <LoginForm />
+      </Route>
+
+      {/*Sign Up Form*/}
+      <Route path='/signup'>
+        <SignUpForm />
+      </Route>
+      
     </div>
   );
 }
