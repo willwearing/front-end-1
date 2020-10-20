81daf6b6e1bbfdceb30c781c4305358275b1a116
Daniel Vargas <davarg5@yahoo.com>
2020-10-20 17:56:30 -0400

    got rid of App.js extra code

--- src/App.js
+++ src/App.js
@@ -25,80 +25,7 @@ const NavLinks = styled.div`
 
 function App() {
 
-  const initialFormValues = {
-    name: '',
-    password: ''
-  }
-
-  const initialFormErrors = {
-    name: '',
-    password: ''
-  }
-
-
-  //Slices of state
-  const [formValues, setFormValues] = useState(initialFormValues);
-  const [users, setUsers] = useState([]);
-  const [formErrors, setFormErrors] = useState(initialFormErrors);
-  const [disabled, setDisabled] = useState(true);
-
-  //Form Schema
-  const formSchema = yup.object().shape({
-    name: yup.string().required('Name is Required').min(2, 'Name must be at least 2 characters'),
-    password: yup.string().required('Password is required').min(6, 'Password must be at least 6 characters')
-  })
-
-  const change = (inputName, inputValue) => {
-    yup.reach(formSchema, inputName).validate(inputValue)
-      .then(() => {
-        setFormErrors({
-          ...formErrors,
-          [inputName]: ''
-        })  
-      })
-      .catch(err => {
-        setFormErrors({
-          ...formErrors,
-          [inputName]: err.errors[0]
-        })
-      })
-    setFormValues({
-      ...formValues,
-      [inputName]: inputValue
-    });
-  }
-
-  const postNewUser = newUser => {
-    axios.post('https://reqres.in/api/users', newUser)
-      .then( res => {
-        console.log(res);
-        setUsers([res.data, 
-          ...users]);
-        setFormValues(initialFormValues);
-      })
-      .catch( err => {
-        console.log(err);
-      })
-  }
-  const submit = () => {
-    const newUser = {
-      name: formValues.name.trim(),
-      password: formValues.password.trim()
-    }
-    postNewUser(newUser);
-  }
-
-  const submitLogin = () => {
-    setFormValues(initialFormValues);
-  }
-
-  useEffect(() => {
-    formSchema.isValid(formValues)
-      .then((valid) => {
-        setDisabled(!valid);
-      })
-  }, [formValues])
-
+  
   return (
     <Router>
     <div className="App">
@@ -116,11 +43,11 @@ function App() {
       {/*Login Form*/}
       <Route path='/login'>
         <LoginForm 
-        values={formValues}
-        errors={formErrors}
-        change={change}
-        disabled={disabled}
-        submit={submitLogin}
+        // values={formValues}
+        // errors={formErrors}
+        // change={change}
+        // disabled={disabled}
+        // submit={submitLogin}
         />
       </Route>
 
